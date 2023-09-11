eb2961bb7f8d387dfc025d280458c98038e6dd1e
kaburia <kaburiaaustin1@gmail.com>
2023-09-11 10:45:35 +0300

    log the errors

--- install.bat
+++ install.bat
@@ -7,13 +7,17 @@ if %errorLevel% neq 0 (
     goto :eof
 )
 
+rem Create a log file
+set LOGFILE=install_log.txt
+echo. > %LOGFILE%
+
 rem Check if Chocolatey is installed
 choco --version >nul 2>&1
 if %errorLevel% neq 0 (
     echo Chocolatey is not installed. Installing Chocolatey...
     
     rem Install Chocolatey
-    @powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))"
+    @powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" 2>> %LOGFILE%
     
     rem Restart the script after Chocolatey installation
     start cmd /k %0
@@ -26,18 +30,19 @@ if %errorLevel% neq 0 (
     echo Node.js is not installed. Installing Node.js...
     
     rem Install Node.js
-    choco install -y nodejs
+    choco install -y nodejs 2>> %LOGFILE%
 )
 rem Install Python3
-choco install -y python
+choco install -y python 2>> %LOGFILE%
 
 rem Install edge-impulse-cli tools
-npm install -g edge-impulse-cli --force
+npm install -g edge-impulse-cli 2>> %LOGFILE%
 
 rem Install Arduino CLI
-@powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/arduino/arduino-cli/master/install.ps1'))"
+@powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/arduino/arduino-cli/master/install.ps1'))" 2>> %LOGFILE%
 
 rem Optional: Notify the user about completion
 echo Installation completed successfully.
+echo Check %LOGFILE% for any installation errors.
 pause
 exit /b 0
