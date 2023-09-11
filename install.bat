1a83565e4bfabf52833776e40fe2783ec7f95acd
kaburia <kaburiaaustin1@gmail.com>
2023-09-11 10:30:28 +0300

    check if choco is installed

--- install.bat
+++ install.bat
@@ -3,11 +3,22 @@ rem Check for admin privileges
 net session >nul 2>&1
 if %errorLevel% neq 0 (
     echo Administrator privileges are required to run this script.
+    pause
     goto :eof
 )
 
-rem Install Chocolatey
-@powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))"
+rem Check if Chocolatey is installed
+choco --version >nul 2>&1
+if %errorLevel% neq 0 (
+    echo Chocolatey is not installed. Installing Chocolatey...
+    
+    rem Install Chocolatey
+    @powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))"
+    
+    rem Restart the script after Chocolatey installation
+    start cmd /k %0
+    exit /b 0
+)
 
 rem Install Python3
 choco install -y python
@@ -23,5 +34,5 @@ rem Install Arduino CLI
 
 rem Optional: Notify the user about completion
 echo Installation completed successfully.
-
+pause
 exit /b 0
