2e740bf9254bd43579854d6e11d06b45318f99da
kaburia <kaburiaaustin1@gmail.com>
2023-09-11 10:42:27 +0300

    check if nodejs is installed

--- install.bat
+++ install.bat
@@ -20,14 +20,19 @@ if %errorLevel% neq 0 (
     exit /b 0
 )
 
+rem Check if Node.js is installed
+node -v >nul 2>&1
+if %errorLevel% neq 0 (
+    echo Node.js is not installed. Installing Node.js...
+    
+    rem Install Node.js
+    choco install -y nodejs
+)
 rem Install Python3
 choco install -y python
 
-rem Install Node.js
-choco install -y nodejs
-
 rem Install edge-impulse-cli tools
-npm install -g edge-impulse-cli
+npm install -g edge-impulse-cli --force
 
 rem Install Arduino CLI
 @powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/arduino/arduino-cli/master/install.ps1'))"
